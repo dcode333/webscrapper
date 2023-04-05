b99ba6ee303a004a282c5887a701c803c597511b
Mohammad Umair <98689944+dcode333@users.noreply.github.com>
2023-04-06 00:45:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+html = '<p>This is a paragraph of text.</p>'
+
+# create a BeautifulSoup object from the HTML
+soup = BeautifulSoup(html, 'html.parser')
+
+# find the <p> tag and extract its text
+p_tag = soup.find('p')
+text = p_tag.text
+
 @app.route('/')
 def index():
-  return jsonify({"name":"umair"})
+  return jsonify(text)
 
 if __name__ == '__main__':
   app.run(port=5000)
